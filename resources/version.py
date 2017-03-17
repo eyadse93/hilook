26e94f1a989eb0cde3236a407c0047c1b7c7ab60
“eyadse93@gmail.com” <eyadse93@gmail.com>
2017-03-18 00:31:40 +0200

    msg

--- resources/version.py
+++ resources/version.py
@@ -1,5 +1,15 @@
 from flask_restful import Resource, reqparse
+from models.version import VersionModel
 
 class AppVersion(Resource):
     def get(self):
-        return {'message': 'this is the app version'}
+        #return {'message': 'this is the app version'}
+
+        if VersionModel.hasRecord() == False:
+            version = VersionModel(1.9, 1.9)
+            version.save_to_db()
+
+        version = VersionModel.find()
+        if version:
+            return version.json()
+        return {'message': 'Item not found'}, 404
