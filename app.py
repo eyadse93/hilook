57fca7a2675fabbb8e8aeec8e5bd89db0d0a5bc9
“eyadse93@gmail.com” <eyadse93@gmail.com>
2017-03-17 23:13:06 +0200

    new commit

--- app.py
+++ app.py
@@ -9,12 +9,10 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from resources.version import AppVersion
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'eyadse93'
+app.secret_key = 'jose'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
@@ -23,10 +21,8 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
-api.add_resource(UserRegister, '/register')
 
-//added
-api.add_resource(AppVersion, '/app_version')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
