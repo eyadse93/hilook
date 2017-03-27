c72519583fd6f41e4fe7c98161253f9b2b14a044
“eyadse93@gmail.com” <eyadse93@gmail.com>
2017-03-27 22:54:21 +0200

    aa

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources.user import UserRegister, UserLogin, GetUserData
+from resources.user import UserRegister, UserLogin, GetUserData, SetUserData
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
