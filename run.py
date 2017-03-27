351221822c2593704d649344ab9c81b2a949c060
“eyadse93@gmail.com” <eyadse93@gmail.com>
2017-03-28 00:22:33 +0200

    aa

--- run.py
+++ run.py
@@ -5,14 +5,18 @@ import threading
 
 
 db.init_app(app)
-# start calling f now and every 60 sec thereafter
-f()
+
 
 def f():
     # do something here ...
     # call f() again in 60 seconds
     print("eeezzz1234321")
     threading.Timer(10, f).start()
+
+# start calling f now and every 60 sec thereafter
+f()
+
+
 
 @app.before_first_request
 def create_tables():
