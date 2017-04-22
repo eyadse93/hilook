cac9b284938ce284ca33498d2077ebc0d2f63b4b
“eyadse93@gmail.com” <eyadse93@gmail.com>
2017-04-23 00:23:30 +0300

    aaa

--- resources/hangouts.py
+++ resources/hangouts.py
@@ -13,4 +13,5 @@ class GetHangouts(Resource):
         data = GetHangouts.parser.parse_args()
         user = UserModel.find_by_username(data['username'])
         if user:
-            
+            result = UserModel.find_hangouts(user.username, user.hangout1, user.hangout2, user.hangout3, user.hangout4, user.country)
+            print (result)
