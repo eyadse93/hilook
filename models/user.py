d2584aa84015d16314c98cb5e087ae23da168393
“eyadse93@gmail.com” <eyadse93@gmail.com>
2017-04-23 21:05:39 +0300

    aaa

--- models/user.py
+++ models/user.py
@@ -116,8 +116,7 @@ class UserModel(db.Model):
 
     @classmethod
     def find_hangouts(cls, username, h1, h2, h3, h4, c):
-        h = "hangout1" + ""
-        result = db.engine.execute("SELECT username from users WHERE h = " + h1 +
+        result = db.engine.execute("SELECT username from users WHERE {hangout1} = " + h1 +
         " OR h = " + h2 + ";")
         print(result)
 
